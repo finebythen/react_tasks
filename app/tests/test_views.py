69cfe41304d69ea44dda9f1a8b19cffcf9f72bf6
Finn Then <finnmortenthen@gmail.com>
2022-06-11 10:30:56 +0200

    changed unit test to dry

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -8,7 +8,24 @@ from rest_framework.test import APITestCase
 USER = get_user_model()
 
 
-class MainTaskTest(APITestCase):
+class TestCaseBase(APITestCase):
+    def setUp(self):
+        self.email = 'johndoe@email.com'
+        self.password = 'johndoepassword'
+        self.data = {
+            'email': self.email,
+            'password': self.password
+        }
+        USER.objects.create_user(email=self.email, password=self.password)
+
+    @property
+    def jwt_token(self):
+        response = self.client.post(reverse('token_obtain_pair'), self.data, format='json')
+        token = response.data['access']
+        return {"HTTP_AUTHORIZATION": f"JWT {token}"}
+
+
+class MainTaskTest(TestCaseBase):
     url_verification = reverse('token_obtain_pair')
     url_maintasks = reverse('maintask-list')
 
@@ -28,20 +45,15 @@ class MainTaskTest(APITestCase):
         self.assertTrue('refresh' in response.data)
 
     def test_get_obj(self):
-        response_verification = self.client.post(self.url_verification, self.data, format='json')
-        token = response_verification.data['access']
-        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {token}")
+        self.client.credentials(**self.jwt_token)
         
         response = self.client.get(self.url_maintasks, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-    
-    def test_post_obj(self):
-        response_verification = self.client.post(self.url_verification, self.data, format='json')
-        token = response_verification.data['access']
-        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {token}")
 
+    def test_post_obj(self):
+        self.client.credentials(**self.jwt_token)
         user = USER.objects.get(email='johndoe@email.com')
 
         data = {
