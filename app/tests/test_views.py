7700409680611dab2f09f6120bb553c7a45343d2
Finn Then <finnmortenthen@gmail.com>
2022-06-14 11:32:56 +0200

    updated view test

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -4,8 +4,11 @@ from django.utils import timezone
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from app.models import MainTask
+
 
 USER = get_user_model()
+URL_VERIFICATION = reverse('token_obtain_pair')
 
 
 class TestCaseBase(APITestCase):
@@ -20,25 +23,26 @@ class TestCaseBase(APITestCase):
 
     @property
     def jwt_token(self):
-        response = self.client.post(reverse('token_obtain_pair'), self.data, format='json')
+        response = self.client.post(URL_VERIFICATION, self.data, format='json')
         token = response.data['access']
         return {"HTTP_AUTHORIZATION": f"JWT {token}"}
 
 
-class MainTaskTest(TestCaseBase):
-    url_verification = reverse('token_obtain_pair')
-    url_maintasks = reverse('maintask-list')
-
+class UserAuthTest(TestCaseBase):
     def test_auth_user(self):
-        response = self.client.post(self.url_verification, self.data, format='json')
+        response = self.client.post(URL_VERIFICATION, self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue('access' in response.data)
         self.assertTrue('refresh' in response.data)
 
+
+class MainTaskTest(TestCaseBase):
+    url = reverse('maintask-list')
+
     def test_get_obj(self):
         self.client.credentials(**self.jwt_token)
         
-        response = self.client.get(self.url_maintasks, format='json')
+        response = self.client.get(self.url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -57,7 +61,49 @@ class MainTaskTest(TestCaseBase):
             'title': 'Maintask #042'
         }
 
-        response = self.client.post(self.url_maintasks, data, format='json')
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class SubTaskTest(TestCaseBase):
+    url = reverse('subtask-list')
+
+    def test_get_obj(self):
+        self.client.credentials(**self.jwt_token)
+
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_obj(self):
+        self.client.credentials(**self.jwt_token)
+        user = USER.objects.get(email='johndoe@email.com')
+
+        maintask = MainTask.objects.create(
+            id=2,
+            active=True,
+            slug='maintask-111',
+            created_by=user,
+            created_at=timezone.now(),
+            updated_at=timezone.now(),
+            title='Maintask #111'
+        )
+
+        data = {
+            'id': 1,
+            'active': True,
+            'slug': 'subtask-001',
+            'created_by': user.id,
+            'created_at': timezone.now(),
+            'updated_at': timezone.now(),
+            'maintask': maintask.id,
+            'title': 'Subtask #001'
+        }
+
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
