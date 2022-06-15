9d9ccb043270fcbe74c81e3da3aa4a9cab10fab3
Finn Then <finnmortenthen@gmail.com>
2022-06-15 12:54:21 +0200

    changed view tests for dry

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,10 +1,11 @@
+import json
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.utils import timezone
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from app.models import MainTask
+from app.models import MainTask, SubTask
 
 
 USER = get_user_model()
@@ -19,7 +20,19 @@ class TestCaseBase(APITestCase):
             'email': self.email,
             'password': self.password
         }
+
         USER.objects.create_user(email=self.email, password=self.password)
+        self.user = USER.objects.get(email='johndoe@email.com')
+
+        MainTask.objects.create(
+            id=1,
+            active=True,
+            slug='maintask-42',
+            created_by=self.user,
+            created_at=timezone.now(),
+            updated_at=timezone.now(),
+            title='Maintask #42'
+        )
 
     @property
     def jwt_token(self):
@@ -52,13 +65,13 @@ class MaintaskListTest(TestCaseBase):
         user = USER.objects.get(email='johndoe@email.com')
 
         data = {
-            'id': 1,
+            'id': 2,
             'active': True,
-            'slug': 'maintask-042',
+            'slug': 'maintask-142',
             'created_by': user.id,
             'created_at': timezone.now(),
             'updated_at': timezone.now(),
-            'title': 'Maintask #042'
+            'title': 'Maintask #142'
         }
 
         response = self.client.post(self.url, data, format='json')
@@ -67,43 +80,101 @@ class MaintaskListTest(TestCaseBase):
         self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
-class SubtaskListTest(TestCaseBase):
-    url = reverse('subtask-list')
+class MaintaskDetailTest(TestCaseBase):
+    url = reverse('maintask-detail', args=['maintask-42'])
 
-    def test_get_obj(self):
+    def test_obj_get(self):
         self.client.credentials(**self.jwt_token)
-
         response = self.client.get(self.url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertNotEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_create_obj(self):
+    def test_obj_patch(self):
         self.client.credentials(**self.jwt_token)
-        user = USER.objects.get(email='johndoe@email.com')
+        response = self.client.patch(self.url, kwargs={'active': False}, format='json')
+        response_data = json.loads(response.content)
+        task = MainTask.objects.get(slug='maintask-42')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_data.get('active'), task.active)
 
-        maintask = MainTask.objects.create(
-            id=2,
-            active=True,
-            slug='maintask-111',
-            created_by=user,
-            created_at=timezone.now(),
-            updated_at=timezone.now(),
-            title='Maintask #111'
-        )
+    
+    def test_obj_delete(self):
+        self.client.credentials(**self.jwt_token)
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertNotEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-        data = {
-            'id': 1,
-            'active': True,
-            'slug': 'subtask-001',
-            'created_by': user.id,
-            'created_at': timezone.now(),
-            'updated_at': timezone.now(),
-            'maintask': maintask.id,
-            'title': 'Subtask #001'
-        }
 
-        response = self.client.post(self.url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+# class SubtaskListTest(TestCaseBase):
+#     url = reverse('subtask-list')
+
+#     def test_get_obj(self):
+#         self.client.credentials(**self.jwt_token)
+
+#         response = self.client.get(self.url, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+#         self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+#     def test_create_obj(self):
+#         self.client.credentials(**self.jwt_token)
+#         user = USER.objects.get(email='johndoe@email.com')
+
+#         maintask = MainTask.objects.create(
+#             id=2,
+#             active=True,
+#             slug='maintask-111',
+#             created_by=user,
+#             created_at=timezone.now(),
+#             updated_at=timezone.now(),
+#             title='Maintask #111'
+#         )
+
+#         data = {
+#             'id': 1,
+#             'active': True,
+#             'slug': 'subtask-001',
+#             'created_by': user.id,
+#             'created_at': timezone.now(),
+#             'updated_at': timezone.now(),
+#             'maintask': maintask.id,
+#             'title': 'Subtask #001'
+#         }
+
+#         response = self.client.post(self.url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+#         self.assertNotEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+# class SubtaskDetailTest(TestCaseBase):
+#     url = reverse('subtask-detail', args=['subtask-242'])
+
+#     def test_obj_get(self):
+#         self.client.credentials(**self.jwt_token)
+#         user = USER.objects.get(email='johndoe@email.com')        
+        
+#         maintask = MainTask.objects.create(
+#             id=5,
+#             active=True,
+#             slug='maintask-888',
+#             created_by=user,
+#             created_at=timezone.now(),
+#             updated_at=timezone.now(),
+#             title='Maintask #888'
+#         )
+
+#         SubTask.objects.create(
+#             id=6,
+#             active=True,
+#             slug='subtask-242',
+#             created_by=user,
+#             created_at=timezone.now(),
+#             updated_at=timezone.now(),
+#             maintask=maintask,
+#             title='Subtask #242',            
+#         )
+
+#         response = self.client.get(self.url, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         self.assertEqual(SubTask.objects.count(), 1)
